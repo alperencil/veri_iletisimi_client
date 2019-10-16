af862f8ac9eab4371d554129404584b2590002f9
alperencil <56614911+alperencil@users.noreply.github.com>
2019-10-16 04:05:31 +0300

    dosyalar

---
+++ client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a full file path");
+                string fileName = Console.ReadLine();
+
+                TcpClient tcpClient = new TcpClient("192.168.0.58", 27018);
+                Console.WriteLine("Bağlanti kuruldu. dosya gonderiliyor.");
+
+                StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+
+                byte[] bytes = File.ReadAllBytes(fileName);
+
+                sWriter.WriteLine(bytes.Length.ToString());
+                sWriter.Flush();
+
+                sWriter.WriteLine(fileName);
+                sWriter.Flush();
+
+                Console.WriteLine("Dosya gönderildi.");
+                tcpClient.Client.SendFile(fileName);
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
